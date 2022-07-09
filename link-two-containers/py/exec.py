b94e383da73fec03f86fbad64686eded247d17d6
Luis Arandas <Luis Arandas@DESKTOP-CEMQMAV.local>
2022-07-10 00:28:59 +0100

    Update containerlink.toe and exec.py

--- link-two-containers/py/exec.py
+++ link-two-containers/py/exec.py
@@ -4,5 +4,11 @@
 
 def onValueChange(channel, sampleIndex, val, prev):
 	value = int(val)
-	print('[-] radio', value)
+	print('[-] this is toggled from container1', value)
+	if value == 1:
+		op('/project1/container2/movie').par.reload = 1
+		op('/project1/container2/movie').par.play = 1
+	else:
+		op('/project1/container2/movie').par.reload = 0
+		op('/project1/container2/movie').par.play = 0
 	return
